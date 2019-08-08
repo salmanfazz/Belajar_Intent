fb5de22672db5d2f9933371e02fb4b835c44cf56
salmanfazz <salman.fazzz@gmail.com>
2019-08-08 12:58:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/belajarintent/IntentActivity.java
@@ -0,0 +1,15 @@
+package com.example.belajarintent;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+
+        // TODO Auto-generated method stub
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+}
